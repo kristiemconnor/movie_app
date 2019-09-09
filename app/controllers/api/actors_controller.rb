01baaf7db6b0134b7f30bbead64b739fe886e757
dani <dani.zaghian@gmail.com>
2019-09-09 11:26:08 -0700

    adds actors and movies with migrations

---
+++ app/controllers/api/actors_controller.rb
@@ -0,0 +1,49 @@
+class Api::ActorsController < ApplicationController
+  def index
+    @actors = Actor.all
+    render 'index.json.jb'
+  end
+
+  def show
+    @actor = Actor.find(params[:id])
+    render 'show.json.jb'
+  end
+
+  def create
+    @actor = Actor.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      known_for: params[:known_for],
+      gender: params[:gender],
+      age: params[:age]
+    )
+
+    if @actor.save
+      render 'show.json.jb'
+    else
+      render json: {errors: @actor.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @actor = Actor.find(params[:id])
+    
+    @actor.first_name = params[:first_name] || @actor.first_name
+    @actor.last_name = params[:last_name] || @actor.last_name
+    @actor.known_for = params[:known_for] || @actor.known_for
+    @actor.gender = params[:gender] || @actor.gender
+    @actor.age = params[:age] || @actor.age
+
+    if @actor.save
+      render 'show.json.jb'
+    else
+      render json: {errors: @actor.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @actor = Actor.find(params[:id])
+    @actor.destroy
+    render json: {message: "Actor successfully destroyed"}
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
-  # EXAMPLE HTML ROUTE
-  # get "/photos" => "photos#index"
+  namespace :api do
+    get "/actors" => "actors#index"
+    post "/actors" => "actors#create"
+    get "/actors/:id" => "actors#show"
+    patch "/actors/:id" => "actors#update"
+    delete "/actors/:id" => "actors#destroy"
 
-  # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+    get "/movies" => "movies#index"
+    post "/movies" => "movies#create"
+    get "/movies/:id" => "movies#show"
+    patch "/movies/:id" => "movies#update"
+    delete "/movies/:id" => "movies#destroy"
+  end
 end
